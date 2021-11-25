ae872368c588b24e96f6092d0d31c597da159f4f
anhdn2511 <anhdn2511@gmail.com>
2021-11-25 18:13:28 +0700

    test backend-heroku

--- backend/app/config/db.config.js
+++ backend/app/config/db.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "12345",
-    DB: "db"
+    HOST: "eu-cdbr-west-01.cleardb.com",
+    USER: "b3ef202dc128a6",
+    PASSWORD: "c7c944a0",
+    DB: "heroku_5ec56d7901dfd10"
 };
